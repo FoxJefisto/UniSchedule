7b87f0c7e0a4c0aa2ad5c41d310f8635ad418296
FoxJefisto <foxjefisto@gmail.com>
2022-09-25 22:04:21 +0300

    Add config

---
+++ UniShedule/Telegram/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniShedule.Telegram
+{
+    class Configuration
+    {
+        public readonly static string BotToken = "5674155370:AAHMgeXaWV45iDZkA9lFoVSu3Kk6RIr9NHg";
+    }
+}

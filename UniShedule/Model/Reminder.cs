2505d656cf851bc2fac8254c625bdf72f3599d4a
FoxJefisto <foxjefisto@gmail.com>
2022-09-26 21:28:07 +0300

    add reminder

---
+++ UniShedule/Model/Reminder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace UniShedule.Model
+{
+    class Reminder
+    {
+        public long Id { get; set; }
+        public DateTime RemindTime { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
